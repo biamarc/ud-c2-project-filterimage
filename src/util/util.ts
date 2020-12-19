49e7c16d239afc02cdc82149bd0776c2ca2ab500
Marco B <marco.biagiotti@tilever.com>
2020-12-19 20:17:02 +0100

    implement utility to manipulate image from public url

---
+++ src/util/util.ts
@@ -0,0 +1,57 @@
+import fs from 'fs';
+import path from 'path'
+import Jimp = require('jimp');
+
+
+// save files on www
+console.debug(`__dirname=${__dirname}`);
+const rootPath = path.resolve(__dirname+"/..");
+console.debug(`rootPath=${rootPath}`);
+
+/**
+ *   filterImageFromURL:
+ *   helper function to download, filter, and save the filtered image locally
+ *
+ *   returns the absolute path to the local image
+ *
+ *   INPUTS
+ *   inputURL: string - a publicly accessible url to an image file
+ *
+ *   RETURNS
+ *   an absolute path to a filtered image locally saved file
+ *
+ */
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+    return new Promise(async resolve => {
+        let photo: Jimp;
+        try{
+            photo = await Jimp.read(inputURL);
+        } catch (e) {
+            console.log(`error while reading url with jimp: ${e}`)
+            return resolve(null);
+        }
+        const outputFile: string = rootPath + '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+
+        await photo
+            .resize(256, 256) // resize
+            .quality(60) // set JPEG quality
+            .greyscale() // set greyscale
+            .write(outputFile, () => {
+                resolve( outputFile);
+            });
+    });
+}
+
+/**
+ * deleteLocalFiles:
+ * helper function to delete files on the local disk
+ * useful to cleanup after tasks
+ *
+ * INPUTS
+ * files: Array<string> an array of absolute paths to files
+ */
+export async function deleteLocalFiles(files: Array<string>): Promise<void> {
+    for (let file of files) {
+        fs.unlinkSync(file);
+    }
+}
